c1d24bf3add9bd8b8c16c1dde52bfd7f229264d2
vijts <tsvijaydel@gmail.com>
2024-07-04 16:37:20 +0530

    test1,2,3 modified

--- java_Pgm/test1_print.java
+++ java_Pgm/test1_print.java
@@ -2,7 +2,7 @@ package java_Pgm;
 
 public class test1_print {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hi");
     }
 
 }
